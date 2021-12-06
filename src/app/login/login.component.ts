58e13c533bbc7394222b51e160e37b07b274bb6e
Gia Phat <giaphat17@yahoo.com.vn>
2021-12-06 21:15:20 +0700

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  backend = "http://localhost:81";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  login(){
+    var acc = (document.getElementById("account") as HTMLInputElement)?.value;
+    var pass = (document.getElementById("password")  as HTMLInputElement)?.value;
+    var ghinho = (document.getElementById("ghinho")  as HTMLInputElement)?.checked;
+    console.log(acc+pass);
+    fetch(`${this.backend}/api/login`, {
+      method : "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify({account: acc, pass:pass, ghinho:ghinho})
+    }).then(r=>r.json())
+    .then(rs=>rs.code == "0").then(p=>{
+      location.href = "/user/info";
+    });
+  }
 }
